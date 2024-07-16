4e9f152f2b634e3e6882f0b56679d4633194eea1
Loris Leveque <9601@holbertonstudents.com>
2024-07-16 08:37:36 +0000

    Pushing last changes

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
+void rev_string(char *str)
+{
+	char result[];
+	int i;
+
+	for (i = _strlen(str) - 1; i >= 0; i--)
+	{
+		result + str[i];
+	}
+
+	str = result;
+}
