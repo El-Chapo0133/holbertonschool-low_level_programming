02f538ee0dd14b19a4ef9203dd5d36f7e96ba1f9
Loris Leveque <9601@holbertonstudents.com>
2024-07-16 11:52:22 +0000

    Pushing last changes

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
 void _puts(char *str)
 {
-	write(STDOUT_FILENO, "Yo", 2)
+	write(STDOUT_FILENO, str, _strlen(str));
 }
