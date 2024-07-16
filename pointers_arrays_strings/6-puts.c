eab1beb914137791d10e7e937cc341b4f02e1679
Loris Leveque <9601@holbertonstudents.com>
2024-07-16 12:43:20 +0000

    Pushing last changes

--- pointers_arrays_strings/6-puts.c
+++ pointers_arrays_strings/6-puts.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
 void puts2(char *str)
 {
+	int length = _strlen(str);
+	int i;
 
+
+	for (i = 0; i < length; i = i + 2)
+	{
+		write(STDOUT_FILENO, str[i], 1);
+	}
 }
