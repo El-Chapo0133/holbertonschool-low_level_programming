6912a313382586a7f216291d1a868da623468d9a
Loris Leveque <9601@holbertonstudents.com>
2024-07-16 11:49:44 +0000

    Pushing last changes

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,35 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-/*void sys_exit(int error_code)
-{
-    	asm volatile
-    	(
-        	"syscall"
-        	: 
-        	: "a"(60), "D"(error_code)
-        	: "rcx", "r11", "memory"
-    	);
-}
-
-int sys_write(unsigned fd, const char *buf, unsigned count)
-{
-    	unsigned ret;
-
-    	asm volatile
-    	(
-        	"syscall"
-        	: "=a"(ret)
-        	: "a"(1), "D"(fd), "S"(buf), "d"(count)
-        	: "rcx", "r11", "memory"
-   	);
-    
-   	return ret;
-}*/
 void _puts(char *str)
 {
-	/*sys_write(1, str, sizeof(str));
-   	sys_exit(12);*/
-
-	printf("yo");
+	write(STDOUT_FILEN0, "Yo", 2)
 }
