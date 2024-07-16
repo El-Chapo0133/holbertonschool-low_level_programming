c5249f95ab4774073fbec267f1ed2a039948c4ba
Loris Leveque <9601@holbertonstudents.com>
2024-07-16 07:48:32 +0000

    Pushing last changes

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -6,7 +6,7 @@
  * It comments things people can read
  * But it is mendatory for some reason
  * So i'll copy this everywhere i need
- * @n - First parameter
+ * @arg1 - First parameter
  *
  * Return - As you can read, it returns void
  */
