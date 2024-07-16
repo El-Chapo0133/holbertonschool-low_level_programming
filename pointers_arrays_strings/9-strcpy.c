a6005a46084f6b63a4b09f49d01127af2cb5e75f
Loris Leveque <9601@holbertonstudents.com>
2024-07-16 09:39:03 +0000

    Pushing last changes

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
+}
+char *_strcpy(char *dest, char *src)
+{
+	int length = _strlen(src);
+
+	for (int i = 0; i < length; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return dest;
+}
