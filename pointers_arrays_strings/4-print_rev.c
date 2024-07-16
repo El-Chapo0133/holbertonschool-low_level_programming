8c6049c2a96c9594e26309cf9369f56c7f666971
Loris Leveque <9601@holbertonstudents.com>
2024-07-16 12:36:29 +0000

    Pushing last changes

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -15,15 +15,13 @@ void print_rev(char *s)
 	int length = _strlen(s);
 	int i, j;
 
+	write(STDOUT_FILENO, s, _strlen(s));
 	for (i = 0, j = length - 1; i < j; i++, j--)
 	{
 		char c = s[i];
 
-		write(STDOUT_FILENO, "yo", 2);
 		s[i] = s[j];
-		write(STDOUT_FILENO, "yo", 2);
 		s[j] = c;
-		write(STDOUT_FILENO, "yo", 2);
 	}
 
 	write(STDOUT_FILENO, s, _strlen(s));
