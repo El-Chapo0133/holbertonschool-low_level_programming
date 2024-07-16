3b56e4c22b4d7962f54632270f7346e75e7b1a11
Loris Leveque <9601@holbertonstudents.com>
2024-07-16 07:42:24 +0000

    Pushing last changes

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 
-/* This is a function description */
+/**
+ * This is a function description for people who can't read
+ * It comments things people can read
+ * But it is mendatory for some reason
+ * So i'll copy this everywhere i need
+ *
+ * Return - As you can read, it returns void
+ */
 void reset_to_98(int *n)
 {
 	*n = 98;
