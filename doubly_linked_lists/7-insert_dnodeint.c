da7c5bd0fc6dc16e16854c891b80b68be7745643
Loris Leveque <9601@holbertonstudents.com>
2024-07-17 13:29:24 +0000

    Pushing last changes

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	int i = 0;
+	dlistint_t *new_node;
+
+	if (head == NULL)
+		return NULL;
+
+	while (head != NULL)
+		head = head->next;
+
+	for (i = 0; head != NULL; i++)
+	{
+		if (i == idx)
+		{
+			dlistint_t *new_node = (struct dlistint_s *)malloc(sizeof(struct dlistint_s));
+			
+			new_node->n = n;
+			new_node->next = head;
+			new_node->prev = head->prev;
+
+			head->prev = new_node;
+			head->prev->next = new_node;
+		}
+	}
+}
