be72c848241135082ffe4c48ab5be63b385ec461
Loris Leveque <9601@holbertonstudents.com>
2024-07-17 11:49:05 +0000

    Pushing last changes

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -5,7 +5,7 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int new_data)
 {
 	dlistint_t *new_node = (struct dlistint_s *)malloc(sizeof(struct dlistint_s));
-	
+
 	if (new_node == NULL)
 	{
 		dprintf(2, "Error, can't malloc\n");
