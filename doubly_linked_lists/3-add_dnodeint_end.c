6d88f04825aed79813630a92426d388c5e293aa9
Loris Leveque <9601@holbertonstudents.com>
2024-07-17 12:35:42 +0000

    Pushing last changes

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -3,11 +3,6 @@
 #include <stdlib.h>
 
 
-/*void move_head_to_end(dlistint_t **head)
-{
-	while ((*head)->next != NULL)
-		(*head) = (*head)->next;
-}*/
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int new_data)
 {
 	dlistint_t *new_node = (struct dlistint_s *)malloc(sizeof(struct dlistint_s));
@@ -17,10 +12,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int new_data)
 		dprintf(2, "Error, can't malloc\n");
 		return (NULL);
 	}
-	/*move_head_to_end(head);*/
-
-	/*while ((*head)->prev != NULL)
-		(*head) = (*head)->prev;*/
 
 	new_node->n = new_data;
 	new_node->prev = (*head);
@@ -28,9 +19,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int new_data)
 
 	if ((*head) != NULL)
 		(*head)->next = new_node;
-	(*head) = new_node;
 	/* Change the pointer to point the new head, which is new_node */
-	/*while ((*head)->next != NULL)
-		(*head) = (*head)->next;*/
+	(*head) = new_node;
 	return (new_node);
 }
