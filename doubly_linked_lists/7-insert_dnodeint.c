a7f1c00fa93393b135298c97cf5f3f5a9430b152
Loris Leveque <9601@holbertonstudents.com>
2024-07-17 13:35:18 +0000

    Pushing last changes

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -25,6 +25,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
 
 			(*head)->prev = new_node;
 			(*head)->prev->next = new_node;
+
+			(*head) = new_node;
+			return new_node;
 		}
 	}
 }
