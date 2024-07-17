a89e9de5a170dd23a99ed3a3609c0a6c22994824
Loris Leveque <9601@holbertonstudents.com>
2024-07-17 13:04:38 +0000

    Pushing last changes

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -13,6 +13,12 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i;
 
+	if (head == NULL)
+		return (NULL);
+
+	while (head->prev != NULL)
+		head = head->prev;
+
 	for (i = 0; head->next != NULL; i++)
 	{
 		if (i == index)
