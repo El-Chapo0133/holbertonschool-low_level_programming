f9665ee35769ac1402c2f42746aaafe6353386d8
Loris Leveque <9601@holbertonstudents.com>
2024-07-17 14:12:10 +0000

    Pushing last changes

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -23,7 +23,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
 			new_node->next = (*head);
 			new_node->prev = (*head)->prev;
 
-			(*head)->prev = new_node;
+			if ((*head) != NULL)
+				(*head)->prev = new_node;
 			if ((*head)->prev != NULL)
 				(*head)->prev->next = new_node;
 
