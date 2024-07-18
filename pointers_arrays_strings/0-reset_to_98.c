77239807bea7e888e414b64566443d58e4030129
Loris Leveque <9601@holbertonstudents.com>
2024-07-18 09:34:13 +0000

    Pushing last changes

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /* This is a short description */
 
 /**
- * This is a function description for people who can't read
+ * reset_to_98 - This is a function description for people who can't read
  * @n: First parameter
  *
  * Return: As you can read, it returns void
