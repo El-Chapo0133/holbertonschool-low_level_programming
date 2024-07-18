c118fc934ee2573d41f1c1dea17cb9b9397f6232
Loris Leveque <9601@holbertonstudents.com>
2024-07-18 09:29:45 +0000

    Pushing last changes

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -6,12 +6,9 @@
 
 /**
  * This is a function description for people who can't read
- * It comments things people can read
- * But it is mendatory for some reason
- * So i'll copy this everywhere i need
- * @arg1 - First parameter
+ * @n: First parameter
  *
- * Return - As you can read, it returns void
+ * Return: As you can read, it returns void
  */
 void reset_to_98(int *n)
 {
